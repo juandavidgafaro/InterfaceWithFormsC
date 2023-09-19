da29324bbe17c90bac32e8a4c7bda43bf5a51154
David Garcia <davidgarcia27737@gmail.com>
2023-09-19 14:02:53 -0500

    Change: Se cambia el icono de la ventana

--- src/App/Form1.h
+++ src/App/Form1.h
@@ -58,6 +58,7 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->labelName = (gcnew System::Windows::Forms::Label());
 			this->labelEdad = (gcnew System::Windows::Forms::Label());
 			this->inputName = (gcnew System::Windows::Forms::TextBox());
@@ -134,6 +135,7 @@ namespace CppCLRWinFormsProject {
 			this->Controls->Add(this->inputName);
 			this->Controls->Add(this->labelEdad);
 			this->Controls->Add(this->labelName);
+			this->Icon = gcnew System::Drawing::Icon("C:/Users/David Garcia/Documents/Proyectos .NET/[C++]/Interface/Interface/src/App/user.ico");
 			this->Name = L"Form1";
 			this->Text = L"REGISTER";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
